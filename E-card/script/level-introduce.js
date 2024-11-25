60944499b46dfd7b5591b159db53b4b320b2ef95
Mynamisboseong <wow456869@gmail.com>
2024-11-26 02:58:04 +0900

    content03 ing

--- E-card/script/level-introduce.js
+++ E-card/script/level-introduce.js
@@ -1,16 +1,16 @@
 const images = [
   {
-    src: "../public/img/main_img_01.png",
+    src: "../assets/img/main_img_01.png",
     title: "일상 회화",
     subtitle: "일상에서 자주 사용하는 짧은 표현과 문장들",
   },
   {
-    src: "../public/img/main_img_02.png",
+    src: "../assets/img/main_img_02.png",
     title: "상황별 대화",
     subtitle: "쇼핑, 여행, 레스토랑 등 다양한 상황에서의 대화연습",
   },
   {
-    src: "../public/img/main_img_03.png",
+    src: "../assets/img/main_img_03.png",
     title: "비즈니스 영어",
     subtitle: "비즈니스 상황에서 사용하는 영어, 발표 및 면접 연습",
   },
@@ -35,5 +35,5 @@ function updateContent() {
 
 // 4초마다 업데이트
 window.onload = () => {
-  setInterval(updateContent, 4000)
+  setInterval(updateContent, 5000)
 }
